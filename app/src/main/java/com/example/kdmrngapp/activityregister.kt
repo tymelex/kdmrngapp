d38ebba4db2da3c3798757a4fcc17b294ff93687
luthor628 <abdulrahmanabdulaziz628@gmail.com>
2023-04-03 10:26:22 +0300

    firstandroidapp

---
+++ app/src/main/java/com/example/kdmrngapp/activityregister.kt
@@ -0,0 +1,11 @@
+package com.example.kdmrngapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activityregister : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_activityregister)
+    }
+}
